493c97d03eb0f320c1402a37e5b11af5c4e283a7
Hidenori Miyoshi <miyohide@gmail.com>
2024-06-10 20:37:31 +0900

    不要なimport文を削除

--- src/main/java/com/github/miyohide/App.java
+++ src/main/java/com/github/miyohide/App.java
@@ -2,9 +2,6 @@ package com.github.miyohide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.function.Function;
 
 @SpringBootApplication
 public class App {
